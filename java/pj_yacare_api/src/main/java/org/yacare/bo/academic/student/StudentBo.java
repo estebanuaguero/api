3cbd5168c95ca14d924ef4ca6297d1172c693bc5
Diego Pablo Mansilla <diegopablomansilla@gmail.com>
2016-09-26 11:38:32 -0300

    subiendo un beta jar
    Signed-off-by: Diego Pablo Mansilla <diegopablomansilla@gmail.com>

--- java/pj_yacare_api/src/main/java/org/yacare/bo/academic/student/StudentBo.java
+++ java/pj_yacare_api/src/main/java/org/yacare/bo/academic/student/StudentBo.java
@@ -33,125 +33,127 @@ public class StudentBo extends AbstractBo {
 
 			Student student = (Student) connectionWrapper.findToJsonById(sql,
 					Student.class, personId);
+			
+			student = completeData(student, connectionWrapper);
 
-			if (student != null) {
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_main_address_person_by_person_id(?);";
-
-				Address mainAddres = (Address) connectionWrapper
-						.findToJsonById(sql, Address.class, personId);
-
-				student.getPersonalInformation().getCommunicationOptions()
-						.getAddresses().setMainAddress(mainAddres);
-
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_alternative_addresses_person_by_person_id(?);";
-
-				List<Object> list = connectionWrapper.findToJsonArray(sql,
-						Address.class, personId);
-
-				List<Address> alternativeAddresses = new ArrayList<Address>();
-
-				for (Object o : list) {
-					alternativeAddresses.add((Address) o);
-				}
-
-				student.getPersonalInformation().getCommunicationOptions()
-						.getAddresses()
-						.setAlternativeAddresses(alternativeAddresses);
-
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_main_email_person_by_person_id(?);";
-
-				Email mainEmail = (Email) connectionWrapper.findToJsonById(sql,
-						Email.class, personId);
-
-				student.getPersonalInformation().getCommunicationOptions()
-						.getEmails().setMainEmail(mainEmail);
-
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_alternative_emails_person_by_person_id(?);";
-
-				list = connectionWrapper.findToJsonArray(sql, Email.class,
-						personId);
-
-				List<Email> alternativeEmails = new ArrayList<Email>();
-
-				for (Object o : list) {
-					alternativeEmails.add((Email) o);
-				}
-
-				student.getPersonalInformation().getCommunicationOptions()
-						.getEmails().setAlternativeEmails(alternativeEmails);
-
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_main_phone_person_by_person_id(?);";
-
-				Phone mainPhone = (Phone) connectionWrapper.findToJsonById(sql,
-						Phone.class, personId);
-
-				student.getPersonalInformation().getCommunicationOptions()
-						.getPhones().setMainPhone(mainPhone);
-
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_alternative_phones_person_by_person_id(?);";
-
-				list = connectionWrapper.findToJsonArray(sql, Phone.class,
-						personId);
-
-				List<Phone> alternativePhones = new ArrayList<Phone>();
-
-				for (Object o : list) {
-					alternativePhones.add((Phone) o);
-				}
-
-				student.getPersonalInformation().getCommunicationOptions()
-						.getPhones().setAlternativePhones(alternativePhones);
-
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_student_responsible_family(?);";
-
-				list = connectionWrapper.findToJsonArray(sql,
-						FamilyLegalGuardian.class, personId);
-
-				List<FamilyLegalGuardian> legalGuardians = new ArrayList<FamilyLegalGuardian>();
-
-				for (Object o : list) {
-					legalGuardians.add((FamilyLegalGuardian) o);
-				}
-
-				student.setLegalGuardians(legalGuardians);
-				
-				// ----------------------------------
-
-				sql = "SELECT * FROM yacare.f_emergency_contacts_by_person_id(?);";
-
-				list = connectionWrapper.findToJsonArray(sql,
-						EmergencyContact.class, personId);
-
-				List<EmergencyContact> emergencyContacts = new ArrayList<EmergencyContact>();
-
-				for (Object o : list) {
-					emergencyContacts.add((EmergencyContact) o);
-				}
-
-				student.setEmergencyContacts(emergencyContacts);
-
-			}
+//			if (student != null) {
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_main_address_person_by_person_id(?);";
+//
+//				Address mainAddres = (Address) connectionWrapper
+//						.findToJsonById(sql, Address.class, personId);
+//
+//				student.getPersonalInformation().getCommunicationOptions()
+//						.getAddresses().setMainAddress(mainAddres);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_alternative_addresses_person_by_person_id(?);";
+//
+//				List<Object> list = connectionWrapper.findToJsonArray(sql,
+//						Address.class, personId);
+//
+//				List<Address> alternativeAddresses = new ArrayList<Address>();
+//
+//				for (Object o : list) {
+//					alternativeAddresses.add((Address) o);
+//				}
+//
+//				student.getPersonalInformation().getCommunicationOptions()
+//						.getAddresses()
+//						.setAlternativeAddresses(alternativeAddresses);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_main_email_person_by_person_id(?);";
+//
+//				Email mainEmail = (Email) connectionWrapper.findToJsonById(sql,
+//						Email.class, personId);
+//
+//				student.getPersonalInformation().getCommunicationOptions()
+//						.getEmails().setMainEmail(mainEmail);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_alternative_emails_person_by_person_id(?);";
+//
+//				list = connectionWrapper.findToJsonArray(sql, Email.class,
+//						personId);
+//
+//				List<Email> alternativeEmails = new ArrayList<Email>();
+//
+//				for (Object o : list) {
+//					alternativeEmails.add((Email) o);
+//				}
+//
+//				student.getPersonalInformation().getCommunicationOptions()
+//						.getEmails().setAlternativeEmails(alternativeEmails);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_main_phone_person_by_person_id(?);";
+//
+//				Phone mainPhone = (Phone) connectionWrapper.findToJsonById(sql,
+//						Phone.class, personId);
+//
+//				student.getPersonalInformation().getCommunicationOptions()
+//						.getPhones().setMainPhone(mainPhone);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_alternative_phones_person_by_person_id(?);";
+//
+//				list = connectionWrapper.findToJsonArray(sql, Phone.class,
+//						personId);
+//
+//				List<Phone> alternativePhones = new ArrayList<Phone>();
+//
+//				for (Object o : list) {
+//					alternativePhones.add((Phone) o);
+//				}
+//
+//				student.getPersonalInformation().getCommunicationOptions()
+//						.getPhones().setAlternativePhones(alternativePhones);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_student_responsible_family(?);";
+//
+//				list = connectionWrapper.findToJsonArray(sql,
+//						FamilyLegalGuardian.class, personId);
+//
+//				List<FamilyLegalGuardian> legalGuardians = new ArrayList<FamilyLegalGuardian>();
+//
+//				for (Object o : list) {
+//					legalGuardians.add((FamilyLegalGuardian) o);
+//				}
+//
+//				student.setLegalGuardians(legalGuardians);
+//
+//				// ----------------------------------
+//
+//				sql = "SELECT * FROM yacare.f_emergency_contacts_by_person_id(?);";
+//
+//				list = connectionWrapper.findToJsonArray(sql,
+//						EmergencyContact.class, personId);
+//
+//				List<EmergencyContact> emergencyContacts = new ArrayList<EmergencyContact>();
+//
+//				for (Object o : list) {
+//					emergencyContacts.add((EmergencyContact) o);
+//				}
+//
+//				student.setEmergencyContacts(emergencyContacts);
+//
+//			}
 
 			// --------------------------------------------------------------------------------------------------------
 
 			return student;
 
 		} catch (Exception e) {
-			 e.printStackTrace();
+			e.printStackTrace();
 			throw new RuntimeException(MSG_2);
 
 		} finally {
@@ -193,116 +195,9 @@ public class StudentBo extends AbstractBo {
 			for (Object obj : listO) {
 
 				Student student = (Student) obj;
-				// ///////////////////////////////////////////////////////////
 
-				if (student != null) {
+				student = completeData(student, connectionWrapper);
 
-					Person personalInformation = student
-							.getPersonalInformation();
-
-					String personId = personalInformation.getId();
-
-					// ----------------------------------
-
-					sql = "SELECT * FROM yacare.f_main_address_person_by_person_id(?);";
-
-					Address mainAddres = (Address) connectionWrapper
-							.findToJsonById(sql, Address.class, personId);
-
-					student.getPersonalInformation().getCommunicationOptions()
-							.getAddresses().setMainAddress(mainAddres);
-
-					// ----------------------------------
-
-					sql = "SELECT * FROM yacare.f_alternative_addresses_person_by_person_id(?);";
-
-					List<Object> list = connectionWrapper.findToJsonArray(sql,
-							Address.class, personId);
-
-					List<Address> alternativeAddresses = new ArrayList<Address>();
-
-					for (Object o : list) {
-						alternativeAddresses.add((Address) o);
-					}
-
-					student.getPersonalInformation().getCommunicationOptions()
-							.getAddresses()
-							.setAlternativeAddresses(alternativeAddresses);
-
-					// ----------------------------------
-
-					sql = "SELECT * FROM yacare.f_main_email_person_by_person_id(?);";
-
-					Email mainEmail = (Email) connectionWrapper.findToJsonById(
-							sql, Email.class, personId);
-
-					student.getPersonalInformation().getCommunicationOptions()
-							.getEmails().setMainEmail(mainEmail);
-
-					// ----------------------------------
-
-					sql = "SELECT * FROM yacare.f_alternative_emails_person_by_person_id(?);";
-
-					list = connectionWrapper.findToJsonArray(sql, Email.class,
-							personId);
-
-					List<Email> alternativeEmails = new ArrayList<Email>();
-
-					for (Object o : list) {
-						alternativeEmails.add((Email) o);
-					}
-
-					student.getPersonalInformation().getCommunicationOptions()
-							.getEmails()
-							.setAlternativeEmails(alternativeEmails);
-
-					// ----------------------------------
-
-					sql = "SELECT * FROM yacare.f_main_phone_person_by_person_id(?);";
-
-					Phone mainPhone = (Phone) connectionWrapper.findToJsonById(
-							sql, Phone.class, personId);
-
-					student.getPersonalInformation().getCommunicationOptions()
-							.getPhones().setMainPhone(mainPhone);
-
-					// ----------------------------------
-
-					sql = "SELECT * FROM yacare.f_alternative_phones_person_by_person_id(?);";
-
-					list = connectionWrapper.findToJsonArray(sql, Phone.class,
-							personId);
-
-					List<Phone> alternativePhones = new ArrayList<Phone>();
-
-					for (Object o : list) {
-						alternativePhones.add((Phone) o);
-					}
-
-					student.getPersonalInformation().getCommunicationOptions()
-							.getPhones()
-							.setAlternativePhones(alternativePhones);
-					
-					
-					// ----------------------------------
-					
-					sql = "SELECT * FROM yacare.f_emergency_contacts_by_person_id(?);";
-
-					list = connectionWrapper.findToJsonArray(sql,
-							EmergencyContact.class, personId);
-
-					List<EmergencyContact> emergencyContacts = new ArrayList<EmergencyContact>();
-
-					for (Object o : list) {
-						emergencyContacts.add((EmergencyContact) o);
-					}
-
-					student.setEmergencyContacts(emergencyContacts);
-
-
-				}
-
-				// ///////////////////////////////////////////////////////////
 				students.add(student);
 			}
 
@@ -317,10 +212,11 @@ public class StudentBo extends AbstractBo {
 			throw new RuntimeException(MSG_2);
 		}
 	}
-	
-	public List<AnnualEnrollment> getAnnualEnrollments(String studentId, Boolean lastAdmission) {
 
-		try {			
+	public List<AnnualEnrollment> getAnnualEnrollments(String studentId,
+			Boolean lastAdmission) {
+
+		try {
 
 			ConnectionWrapper connectionWrapper = this.getDataSourceWrapper()
 					.getConnectionWrapper();
@@ -337,7 +233,7 @@ public class StudentBo extends AbstractBo {
 			for (Object obj : listO) {
 
 				AnnualEnrollment annualEnrollment = (AnnualEnrollment) obj;
-				
+
 				annualEnrollments.add(annualEnrollment);
 			}
 
@@ -351,6 +247,119 @@ public class StudentBo extends AbstractBo {
 			e.printStackTrace();
 			throw new RuntimeException(MSG_2);
 		}
+	}
+
+	private Student completeData(Student student,
+			ConnectionWrapper connectionWrapper) {
+
+		// ///////////////////////////////////////////////////////////
+
+		if (student != null) {
+
+			Person personalInformation = student.getPersonalInformation();
+
+			String personId = personalInformation.getId();
+
+			// ----------------------------------
+
+			String sql = "SELECT * FROM yacare.f_main_address_person_by_person_id(?);";
+
+			Address mainAddres = (Address) connectionWrapper.findToJsonById(
+					sql, Address.class, personId);
+
+			student.getPersonalInformation().getCommunicationOptions()
+					.getAddresses().setMainAddress(mainAddres);
+
+			// ----------------------------------
+
+			sql = "SELECT * FROM yacare.f_alternative_addresses_person_by_person_id(?);";
+
+			List<Object> list = connectionWrapper.findToJsonArray(sql,
+					Address.class, personId);
+
+			List<Address> alternativeAddresses = new ArrayList<Address>();
+
+			for (Object o : list) {
+				alternativeAddresses.add((Address) o);
+			}
+
+			student.getPersonalInformation().getCommunicationOptions()
+					.getAddresses()
+					.setAlternativeAddresses(alternativeAddresses);
+
+			// ----------------------------------
+
+			sql = "SELECT * FROM yacare.f_main_email_person_by_person_id(?);";
+
+			Email mainEmail = (Email) connectionWrapper.findToJsonById(sql,
+					Email.class, personId);
+
+			student.getPersonalInformation().getCommunicationOptions()
+					.getEmails().setMainEmail(mainEmail);
+
+			// ----------------------------------
+
+			sql = "SELECT * FROM yacare.f_alternative_emails_person_by_person_id(?);";
+
+			list = connectionWrapper
+					.findToJsonArray(sql, Email.class, personId);
+
+			List<Email> alternativeEmails = new ArrayList<Email>();
+
+			for (Object o : list) {
+				alternativeEmails.add((Email) o);
+			}
+
+			student.getPersonalInformation().getCommunicationOptions()
+					.getEmails().setAlternativeEmails(alternativeEmails);
+
+			// ----------------------------------
+
+			sql = "SELECT * FROM yacare.f_main_phone_person_by_person_id(?);";
+
+			Phone mainPhone = (Phone) connectionWrapper.findToJsonById(sql,
+					Phone.class, personId);
+
+			student.getPersonalInformation().getCommunicationOptions()
+					.getPhones().setMainPhone(mainPhone);
+
+			// ----------------------------------
+
+			sql = "SELECT * FROM yacare.f_alternative_phones_person_by_person_id(?);";
+
+			list = connectionWrapper
+					.findToJsonArray(sql, Phone.class, personId);
+
+			List<Phone> alternativePhones = new ArrayList<Phone>();
+
+			for (Object o : list) {
+				alternativePhones.add((Phone) o);
+			}
+
+			student.getPersonalInformation().getCommunicationOptions()
+					.getPhones().setAlternativePhones(alternativePhones);
+
+			// ----------------------------------
+
+			sql = "SELECT * FROM yacare.f_emergency_contacts_by_person_id(?);";
+
+			list = connectionWrapper.findToJsonArray(sql,
+					EmergencyContact.class, personId);
+
+			List<EmergencyContact> emergencyContacts = new ArrayList<EmergencyContact>();
+
+			for (Object o : list) {
+				emergencyContacts.add((EmergencyContact) o);
+			}
+
+			student.setEmergencyContacts(emergencyContacts);
+
+		}
+
+		// ///////////////////////////////////////////////////////////
+
+		return student;
+
 	}
 
 }
